2211e25c1f56b38e78079020a3ced547252f91f1
skySungw <958408111@qq.com>
2021-04-29 15:44:35 +0800

    test

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@
 git add .;
 git commit -m $1;
 git push origin main;
-# npm run lernatest;
-lerna publish
+npm run lernatest;
+# lerna publish
 # lerna create testui 
 # cd testui 
 # lerna add testsgw
