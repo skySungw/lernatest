caec6690488dcb9f5b5380fe64d2c899bb4c43a5
skySungw <958408111@qq.com>
2021-04-29 15:20:29 +0800

    发新包

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-rm -rf packages;
-mkdir packages;
+# rm -rf packages;
+# mkdir packages;
 
 git add .;
 git commit -m $1;
 git push origin main;
 npm run lernatest;
 echo "完成";
-node ./index.js;
 exit;
